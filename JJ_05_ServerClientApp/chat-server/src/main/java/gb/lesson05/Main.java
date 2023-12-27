4006f8763be6ed5dc4245e9c706a36bb5c6125c7
kvo52@kvo1 <kvo.52@mail.ru>
2023-12-27 17:16:06 +0300

    Progect_05_ChatDemo_new classes

--- JJ_05_ServerClientApp/chat-server/src/main/java/gb/lesson05/Main.java
+++ JJ_05_ServerClientApp/chat-server/src/main/java/gb/lesson05/Main.java
@@ -1,11 +1,25 @@
 package gb.lesson05;
 
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.rmi.UnknownHostException;
+
 public class Main {
+
     public static void main(String[] args) {
-
-        System.out.printf("Hello Server!");
-
-
+        try
+        {
+            ServerSocket serverSocket = new ServerSocket(1400);
+            Server server = new Server(serverSocket);
+            server.runServer();
+        }
+        catch (UnknownHostException e){
+            e.printStackTrace();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
     }
+
 }
