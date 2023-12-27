2f986c83be9709e880d3803e6bcb82f00c9cdb99
kvo52@kvo1 <kvo.52@mail.ru>
2023-12-27 10:45:35 +0300

    Progect_05_ChatDemo_structures

---
+++ JJ_05_ServerClientApp/chat-client/src/main/java/gb/lesson05/Main.java
@@ -0,0 +1,9 @@
+package gb.lesson05;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.printf("Hello Client!");
+
+    }
+}
---
+++ JJ_05_ServerClientApp/chat-server/src/main/java/gb/lesson05/Main.java
@@ -0,0 +1,11 @@
+package gb.lesson05;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.printf("Hello Server!");
+
+
+    }
+}
